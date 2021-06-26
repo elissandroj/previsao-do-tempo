cfaeb4e2eda3bf960da11119706f1a1aa11b9f6c
Elissandro José <elissandrojose@hotmail.com>
2021-06-25 23:31:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Previsão do Tempo
+Criação de um projeto para consultar a previsão do tempo via API com Angular
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: '837bd9943e64360518c252bea42bfc1f',
+};
